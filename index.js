c1364ee36d1ad714935f23be0a0ba291314af79e
rkadesign17 <rkaripkinabila@gmail.com>
2025-02-22 23:25:43 +0700

    index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
+const BotName = 'Vvipripki-bot'; 
 const instagram = 'agus_alnsyh71- kaelMk_'; 
 const aktif = 'Kapan bot aktif';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
